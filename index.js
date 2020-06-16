48480151d76ea8533bcccc5b64ca1a6cf559ef9b
Marin37 <marijmotta@gmail.com>
2020-06-16 20:46:57 -0300

    Introduciendo app a Heroku

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.get('/',(req, res)=> {
 res.send('Hello Github Pages'));
 });
 
-app.listen(3000);
+app.listen(port);
