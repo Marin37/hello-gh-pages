945816057acdfe66e8aba50cdf30457cb8a45875
Marin37 <marijmotta@gmail.com>
2020-06-17 12:58:39 -0300

    EEgg Final

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 function convert()
 {
-let celsius = parseFloat(document.getElementById('tc').value);
+let bruh == false
+let celsius = document.getElementById('tc').value
+if (celsius == 'si')
+{
+  bruh = true
+}
+celsius = parseFloat(document.getElementById('tc').value);
 let fahrenheit = ((celsius * 1.8) + 32).toFixed(2);
 let kelvin = (celsius + 273.15).toFixed(2);
 if (isNaN(fahrenheit) || isNaN(kelvin))
 {
   fahrenheit = 'FLACO SI NO PONES UN NUMERO';
   kelvin = 'NO PUEDO HACER NADA';
+};
+if (bruh == true) {
+  fahrenheit = 'si @jeico704';
+  kelvin = 'si @angel2112';
 };
 document.getElementById('tf').innerHTML = fahrenheit;
 document.getElementById('tk').innerHTML = kelvin;
