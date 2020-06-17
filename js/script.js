1fc75d8217709a17168f9fbad5e3b1fea5de5642
Marin37 <marijmotta@gmail.com>
2020-06-17 11:43:06 -0300

    ee4

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 function convert()
 {
 let celsius = parseFloat(document.getElementById('tc').value);
-if (celsius == 'NaN')
+if (celsius == NaN)
 {
-  document.getElementById('tk').innerHTML = 'FLACO SI NO PONES UN NUMERO'
-  document.getElementById('tf').innerHTML = 'NO PUEDO HACER NADA PELELE'
+  document.getElementById('tf').innerHTML = 'FLACO SI NO PONES UN NUMERO';
+  document.getElementById('tk').innerHTML = 'NO PUEDO HACER NADA PELELE';
 }
 let fahrenheit = ((celsius * 1.8) + 32).toFixed(2);
 let kelvin = (celsius + 273.15).toFixed(2);
 
-document.getElementById('tk').innerHTML = kelvin;
 document.getElementById('tf').innerHTML = fahrenheit;
+document.getElementById('tk').innerHTML = kelvin;
+
 }
