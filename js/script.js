733bf6ffd8a83607d710acd7a776379f9b2a5696
Marin37 <marijmotta@gmail.com>
2020-06-17 11:50:47 -0300

    Mira master la verdad no se cuantos voy pero solo espero que esto sea lo ultimo

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 function convert()
 {
 let celsius = parseFloat(document.getElementById('tc').value);
-if (celsius == NaN)
+let fahrenheit = ((celsius * 1.8) + 32).toFixed(2);
+let kelvin = (celsius + 273.15).toFixed(2);
+if (isNaN(celsius))
 {
   document.getElementById('tf').innerHTML = 'FLACO SI NO PONES UN NUMERO';
   document.getElementById('tk').innerHTML = 'NO PUEDO HACER NADA PELELE';
-}
-let fahrenheit = ((celsius * 1.8) + 32).toFixed(2);
-let kelvin = (celsius + 273.15).toFixed(2);
-
+};
 document.getElementById('tf').innerHTML = fahrenheit;
 document.getElementById('tk').innerHTML = kelvin;
-
 }
