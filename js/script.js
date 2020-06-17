c8e90e028eaba7ac5009f6f41fe45266e1d5a55e
Marin37 <marijmotta@gmail.com>
2020-06-17 12:06:26 -0300

    si no funca lo dejo

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ function convert()
 let celsius = parseFloat(document.getElementById('tc').value);
 let fahrenheit = ((celsius * 1.8) + 32).toFixed(2);
 let kelvin = (celsius + 273.15).toFixed(2);
-if ((isNaN(fahrenheit) == true) || (isNaN(kelvin) == true))
+if (isNaN(fahrenheit) || isNaN(kelvin))
 {
   fahrenheit = 'FLACO SI NO PONES UN NUMERO';
-  kelvin = 'NO PUEDO HACER NADA PELELE';
+  kelvin = 'NO PUEDO HACER NADA';
 };
 document.getElementById('tf').innerHTML = fahrenheit;
 document.getElementById('tk').innerHTML = kelvin;
