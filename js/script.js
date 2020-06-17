1dbf0bc031af5db9494053f8405e5bc17cb5bfde
Marin37 <marijmotta@gmail.com>
2020-06-17 11:19:24 -0300

    easter egg

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 function convert()
 {
 let celsius = parseFloat(document.getElementById('tc').value);
+if (celsius == 'si')
+{
+  document.getElementById('tk').innerHTML = 'FLACO SI NO PONES UN NUMERO'
+  document.getElementById('tf').innerHTML = 'NO PUEDO HACER NADA PELELE'
+}
 let fahrenheit = ((celsius * 1.8) + 32).toFixed(2);
 let kelvin = (celsius + 273.15).toFixed(2);
 
