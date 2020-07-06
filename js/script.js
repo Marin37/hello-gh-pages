895ae1dc25b3eefddadae8b67df0b8927bae327e
Marin37 <marijmotta@gmail.com>
2020-07-06 17:09:43 -0300

    Revert "Anotaciones en el js"
    This reverts commit 0c1aba93bab9a93c40857736ec56863dca20a73b.

--- js/script.js
+++ js/script.js
@@ -7,25 +7,18 @@ let bruh4 = false
 let bruh5 = false
 let celsius = document.getElementById('tc').value
 
-//TRIGGERS EASTER EGGS
-if (celsius == 'si' || celsius == 'Si' || celsius == 'SI')
+if (celsius == 'si')
 {bruh1 = true}
-if (celsius == 'sale lol?' ||
-celsius == 'Sale lol?' ||
-celsius == 'sale lolcito?' ||
-celsius == 'Sale lolcito?' ||
-celsius == 'sale normal?' ||
-celsius == 'Sale normal?'
-)
+if (celsius == 'sale lol?')
 {bruh2 = true}
-if (celsius == 'pc' || celsius == 'PC')
+if (celsius == 'pc')
 {bruh3 = true}
-if (celsius == "Cho'Gath")
+if (celsius == "Cho'gath")
 {bruh4 = true}
 if (celsius == 'Santiago Trini')
 {bruh5 = true}
 
-//FORMULA DE VERDAD
+
 celsius = parseFloat(document.getElementById('tc').value);
 let fahrenheit = ((celsius * 1.8) + 32).toFixed(2);
 let kelvin = (celsius + 273.15).toFixed(2);
@@ -35,29 +28,26 @@ if (isNaN(fahrenheit) || isNaN(kelvin))
   kelvin = 'NO PUEDO HACER NADA';
 };
 
-//EASTER EGGS
 if (bruh1 == true) {
-  fahrenheit = 'si @jeico704 ';
-  kelvin = 'si @angel2112 ';
+  fahrenheit = 'si @jeico704';
+  kelvin = 'si @angel2112';
 };
 if (bruh2 == true) {
-  fahrenheit = 'si master de una ';
-  kelvin = 'anota Marin37#LAS ';
+  fahrenheit = 'si master de una';
+  kelvin = 'anota: Marin37 #LAS';
 };
 if (bruh3 == true) {
-  fahrenheit = 'MARISA-PC ';
-  kelvin = 'Esa es la pc que corre lol a 15 fps ';
+  fahrenheit = 'MARISA-PC';
+  kelvin = 'Esa es la pc que corre lol a 15 fps';
 };
 if (bruh4 == true) {
-  fahrenheit = 'Buenardo el main ';
-  kelvin = 'Chorongat M7 100k puntos ';
+  fahrenheit = 'Buenardo el main';
+  kelvin = 'Chorongat M7 100k puntos';
 };
 if (bruh5 == true) {
-  fahrenheit = 'Alto capo el profe ';
-  kelvin = 'Agradecido con @SantiagoTrini ';
+  fahrenheit = 'Alto capo el profe';
+  kelvin = 'Agradecido con @SantiagoTrini';
 };
-
-//PARA QUE ANDE NORMAL
 document.getElementById('tf').innerHTML = fahrenheit;
 document.getElementById('tk').innerHTML = kelvin;
 }
