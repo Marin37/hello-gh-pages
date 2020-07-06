51e7452ff73e1220a21ec79c3a81e0ab78057322
Marin37 <marijmotta@gmail.com>
2020-07-06 17:07:26 -0300

    Revert "volviendo a como estambamos2"
    This reverts commit 3510d5ca11e15e13baff3b3e54c0eb97e0d55baf.

--- js/script.js
+++ js/script.js
@@ -135,7 +135,9 @@ document.getElementById('tk').innerHTML = kelvin;
 
 /* function convertF()
 {
-let fahrenheit = parseFloat(document.getElementById('tf').value);
+  let fahrenheit = document.getElementById('tf2').value
+  fahrenheit = parseFloat(document.getElementById('tf2').value);
+
 let celsius = ((fahrenheit - 32) / 1.8).toFixed(2);
 let kelvin = (celsius + 273.15).toFixed(2);
 if (isNaN(celsius) || isNaN(kelvin))
@@ -143,7 +145,7 @@ if (isNaN(celsius) || isNaN(kelvin))
   fahrenheit = 'FLACO SI NO PONES UN NUMERO';
   kelvin = 'NO PUEDO HACER NADA';
 };
-document.getElementById('tc').innerHTML = celsius;
-document.getElementById('tk').innerHTML = kelvin;
+document.getElementById('tc2').innerHTML = celsius;
+document.getElementById('tk2').innerHTML = kelvin;
 }
 */
