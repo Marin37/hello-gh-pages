06ce7342c11109a31c3d4319f2aed1ac2e22b77b
Marin37 <marijmotta@gmail.com>
2020-07-06 17:07:31 -0300

    Revert "probando si es posible"
    This reverts commit 3f7d055aaa3d10ea6d684819b56640c8d5d8a13f.

--- js/script.js
+++ js/script.js
@@ -62,78 +62,7 @@ document.getElementById('tf').innerHTML = fahrenheit;
 document.getElementById('tk').innerHTML = kelvin;
 }
 
-function convertC()
-{
-let bruh1 = false
-let bruh2 = false
-let bruh3 = false
-let bruh4 = false
-let bruh5 = false
-let celsius = document.getElementById('tc').value
-
-//TRIGGERS EASTER EGGS
-if (celsius == 'si' || celsius == 'Si' || celsius == 'SI')
-{bruh1 = true}
-if (celsius == 'sale lol?' ||
-celsius == 'Sale lol?' ||
-celsius == 'sale lolcito?' ||
-celsius == 'Sale lolcito?' ||
-celsius == 'sale normal?' ||
-celsius == 'Sale normal?'
-)
-{bruh2 = true}
-if (celsius == 'pc' || celsius == 'PC')
-{bruh3 = true}
-if (celsius == "Cho'Gath")
-{bruh4 = true}
-if (celsius == 'Santiago Trini')
-{bruh5 = true}
-
-//FORMULA DE VERDAD
-celsius = parseFloat(document.getElementById('tc').value);
-let fahrenheit = ((celsius * 1.8) + 32).toFixed(2);
-let kelvin = (celsius + 273.15).toFixed(2);
-if (isNaN(fahrenheit) || isNaN(kelvin))
-{
-  fahrenheit = 'FLACO SI NO PONES UN NUMERO';
-  kelvin = 'NO PUEDO HACER NADA';
-};
-
-//EASTER EGGS
-if (bruh1 == true) {
-  fahrenheit = 'si @jeico704 ';
-  kelvin = 'si @angel2112 ';
-};
-if (bruh2 == true) {
-  fahrenheit = 'si master de una ';
-  kelvin = 'anota Marin37#LAS ';
-};
-if (bruh3 == true) {
-  fahrenheit = 'MARISA-PC ';
-  kelvin = 'Esa es la pc que corre lol a 15 fps ';
-};
-if (bruh4 == true) {
-  fahrenheit = 'Buenardo el main ';
-  kelvin = 'Chorongat M7 100k puntos ';
-};
-if (bruh5 == true) {
-  fahrenheit = 'Alto capo el profe ';
-  kelvin = 'Agradecido con @SantiagoTrini ';
-};
-
-//PARA QUE ANDE NORMAL
-document.getElementById('tf').innerHTML = fahrenheit;
-document.getElementById('tk').innerHTML = kelvin;
-}
-
-
-
-
-
-
-
-
-/* function convertF()
+function convertF()
 {
   let fahrenheit = document.getElementById('tf2').value
   fahrenheit = parseFloat(document.getElementById('tf2').value);
@@ -148,4 +77,3 @@ if (isNaN(celsius) || isNaN(kelvin))
 document.getElementById('tc2').innerHTML = celsius;
 document.getElementById('tk2').innerHTML = kelvin;
 }
-*/
